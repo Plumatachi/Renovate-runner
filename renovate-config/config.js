4bca20fe7d820c2f4371565d575ace8f58abf4e4
c.maupou <c.maupou@brgm.fr>
2026-04-28 16:59:49 +0200

    Autodiscover enabled

--- renovate-config/config.js
+++ renovate-config/config.js
@@ -1,18 +1,16 @@
 module.exports = {
     platform: 'github',
-    autodiscover: false,       // ne pas découvrir automatiquement des dépôts (temporaire)
+    autodiscover: true,       // permet de découvrir automatiquement des dépôts
     autodiscoverFilter: ['Plumatachi/*'],       // permet de filtrer les dépôts à découvrir
-    repositories: [
-        'Plumatachi/bookly',
-        'Plumatachi/SAE_Python',
-        'Plumatachi/Space_Invaders'
-    ],
+    // repositories: [
+    //     'Plumatachi/bookly',
+    // ],
     onboarding: false,
     requireConfig: 'optional',
     automerge: false,          // validation manuelle des MR
     labels: ['renovate'],
     logLevel: 'debug',
-    recreateClosed: true,      // permet de re-créer les MR fermées (seulement pour les tests)
+    recreateClosed: true,      // permet de re-créer les MR fermées (actif seulement pour les tests)
 
     // Fréquence : une fois par semaine
     schedule: ['on monday'],
